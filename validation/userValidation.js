b13cf4d8c819b8572377e7b583e773a8a8ee0a4c
Acengen <kocic999@yahoo.com>
2022-12-25 21:33:12 +0100

    user email Validation fix

--- validation/userValidation.js
+++ validation/userValidation.js
@@ -1,9 +1,11 @@
 exports.validateEmail = (value, { req }) => {
   if (!value) {
     return Promise.reject("'Email' field is required");
-  } else if (!value.includes("@") || !value.includes(".")) {
+  } else if (!value.includes("@")) {
     return Promise.reject(
       "'Email' field is invalid, need to have @ and email domain exp:email@domain.com"
     );
+  } else {
+    return Promise.resolve();
   }
 };
