3ae185b93b5ddcb7b105299dc7b12f3923496710
Acengen <kocic999@yahoo.com>
2023-01-27 19:42:29 +0100

    user model clean ups

--- model/user.js
+++ model/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const calculations = require("../helpers/calcs")
+const calculations = require("../helpers/calcs");
 const Schema = mongoose.Schema;
 
 const userShema = new Schema({
@@ -48,29 +48,44 @@ const userShema = new Schema({
 });
 
 userShema.methods.calculateCosts = function (req) {
-  //Updated smoking info
   if (req.savedInfo) {
     this.savedInfo.packCigarettesPrice = req.savedInfo.packCigarettesPrice;
     this.savedInfo.cigarettesInPack = req.savedInfo.cigarettesInPack;
     this.savedInfo.cigarettesDay = req.savedInfo.cigarettesDay;
     this.savedInfo.cigarettesAvoided = req.savedInfo.cigarettesAvoided;
+
+    this.savedInfo.cigarettesDailyCost = calculations.cigDailyCosts(
+      req.savedInfo
+    );
+    this.savedInfo.cigarettesMontlyCost = calculations.cigMontlyCost(
+      req.savedInfo
+    );
+    this.savedInfo.cigarettesYearlyCost = calculations.cigYearlyCost(
+      req.savedInfo
+    );
+    this.savedInfo.cigarettesAvoidedCost = calculations.cigAvoidedCost(
+      req.savedInfo,
+      this.savedInfo.cigarettesAvoided
+    );
   } else {
     this.smokingInfo.cigarettesAvoided = req.smokingInfo.cigarettesAvoided;
     this.smokingInfo.packCigarettesPrice = req.smokingInfo.packCigarettesPrice;
     this.smokingInfo.cigarettesInPack = req.smokingInfo.cigarettesInPack;
     this.smokingInfo.cigarettesDay = req.smokingInfo.cigarettesDay;
-  }
 
-  if (!req.savedInfo) {
-    this.smokingInfo.cigarettesDailyCost = calculations.cigDailyCosts(req.smokingInfo)
-    this.smokingInfo.cigarettesMontlyCost = calculations.cigMontlyCost(req.smokingInfo)
-    this.smokingInfo.cigarettesYearlyCost = calculations.cigYearlyCost(req.smokingInfo)
-    this.smokingInfo.cigarettesAvoidedCost = calculations.cigAvoidedCost(req.smokingInfo,this.smokingInfo.cigarettesAvoided)
-  } else {
-    this.savedInfo.cigarettesDailyCost = calculations.cigDailyCosts(req.savedInfo)
-    this.savedInfo.cigarettesMontlyCost = calculations.cigMontlyCost(req.savedInfo)
-    this.savedInfo.cigarettesYearlyCost = calculations.cigYearlyCost(req.savedInfo)
-    this.savedInfo.cigarettesAvoidedCost = calculations.cigAvoidedCost(req.savedInfo,this.savedInfo.cigarettesAvoided)
+    this.smokingInfo.cigarettesDailyCost = calculations.cigDailyCosts(
+      req.smokingInfo
+    );
+    this.smokingInfo.cigarettesMontlyCost = calculations.cigMontlyCost(
+      req.smokingInfo
+    );
+    this.smokingInfo.cigarettesYearlyCost = calculations.cigYearlyCost(
+      req.smokingInfo
+    );
+    this.smokingInfo.cigarettesAvoidedCost = calculations.cigAvoidedCost(
+      req.smokingInfo,
+      this.smokingInfo.cigarettesAvoided
+    );
   }
 
   return this.save();
