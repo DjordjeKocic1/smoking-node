d8ae4e3d533d6fe76a579f2fd56aa002792c650a
Acengen <kocic999@yahoo.com>
2023-06-12 16:11:20 +0200

    user timestamps

--- dist/model/user.js
+++ dist/model/user.js
@@ -94,7 +94,7 @@ const userShema = new Schema({
         },
     ],
     notificationToken: String,
-});
+}, { timestamps: true });
 userShema.methods.calculateCosts = function (req) {
     if (req.savedInfo) {
         this.savedInfo.packCigarettesPrice = req.savedInfo.packCigarettesPrice;
