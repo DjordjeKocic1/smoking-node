7ab5de74cbcc5a530a5b46c813f1681a471519f0
Acengen <kocic999@yahoo.com>
2023-06-19 21:44:00 +0200

    test

--- dist/helpers/passportStrategies.js
+++ dist/helpers/passportStrategies.js
@@ -6,5 +6,5 @@ exports.google = {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     //todo: based on env, change url to localhost, dev or prod
-    callbackURL: "/auth/google/callback",
+    callbackURL: "http://localhost:8000/auth/google",
 };
