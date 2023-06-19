7f613a05044ed9621397d40ddf379dd388d81f1a
Acengen <kocic999@yahoo.com>
2023-06-19 23:04:05 +0200

    test

--- helpers/passportStrategies.ts
+++ helpers/passportStrategies.ts
@@ -4,5 +4,6 @@ export const google = {
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   //todo: based on env, change url to localhost, dev or prod
-  callbackURL: "/auth/google",
+  callbackURL:
+    "https://whale-app-hkbku.ondigitalocean.app/auth/google/callback",
 };
