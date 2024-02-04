ce188a64e7a87d04f1774c43718e9ae757f6a241
Acengen <kocic999@yahoo.com>
2024-02-04 17:16:51 +0100

    typo fix

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -145,7 +145,7 @@ const createPlan: RequestHandler<IParams, {}, IPlans> = async (
     let plan = new Plans({
       name: req.body.name,
       completed: false,
-      type:req.body.type,
+      type:req.body.userType,
       userId: req.params.id,
     });
 
